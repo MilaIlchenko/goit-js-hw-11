af6eb2ebf58113b09d24e80ef8aa73431f091f22
MilaIlchenko <147260557+MilaIlchenko@users.noreply.github.com>
2024-03-10 16:23:10 +0100

    update

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,38 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const KEY = "34991157-d38b6fef3b34c3f08b128977b";
+const URL = "https://pixabay.com/api/";
+
+const loader = document.querySelector('.loader');
+
+export function searchImages(QUERY) {
+    const LINK = `${URL}?key=${KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&savesearch=true`;
+    
+    loader.style.display = 'block';
+
+    return fetch(LINK)
+    .then(response => {
+        if(!response.ok){
+          throw new Error (`Response error ${response.status}`)
+        }
+        return response.json()
+    })
+    .then(data => {
+
+      loader.style.display = 'none';
+      
+      if (data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error',
+          timeout: 3000,
+          position: 'bottomRight',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      return data;
+    })
+    .catch (error => 
+        console.log(`Error: ${error}`))
+    }
